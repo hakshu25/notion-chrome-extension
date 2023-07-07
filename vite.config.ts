68b088282db8a7cbb94ae7a1df884a7b3eaafa58
hakshu <flyhighup.air25@gmail.com>
2023-07-07 14:40:49 +0900

    build: manifest設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { crx } from '@crxjs/vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    crx({
+      manifest: {
+        manifest_version: 3,
+        name: 'Notion Page Clipper With Tag',
+        version: '1.0.0',
+        action: {
+          default_popup: 'index.html',
+        },
+      },
+    }),
+  ],
+});
